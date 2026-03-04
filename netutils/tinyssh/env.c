d8dd3251b6e630c30ae347f9739c33d1a1f01eb9
Peter Barada <peter.barada@gmail.com>
2026-03-04 12:37:41 -0500

    Provide access to environ[]
    In Nuttx, stdlib.h defines environ as a pointer to function
    that returns pointer to task's copy of environ. Don't declare
    envorn as extern, and include <stdlib.h>.
    
    Signed-off-by: Peter Barada <peter.barada@gmail.com>

--- netutils/tinyssh/env.c
+++ netutils/tinyssh/env.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+
 #include "str.h"
 #include "env.h"
 
--- netutils/tinyssh/env.h
+++ netutils/tinyssh/env.h
@@ -1,13 +1,17 @@
 #ifndef ENV_H____
 #define ENV_H____
 
+#include <stdlib.h>
+
 /*
 The environment 'environ', is a 0-terminated array
 of 0-terminated strings, called environment variables.
 Each environment variable is of the form name=value.
-*/
-extern char **environ;
 
+In Nuttx stdlib.h, environ is a macro to a function that
+returns a pointer to the environment; as such it can't be
+declared as extern.
+-*/
 extern char *env_get(const char *);
 
 #endif
